1b5388e30a557ad169da7672e79f44f1a25a917e
fencyk1 <fencyk.1@buckeyemail.osu.edu>
2010-11-15 11:52:59 -0500

    Skeleton of Linker

--- src/Linker.java
+++ src/Linker.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 
 public class Linker {
 
@@ -5,8 +6,33 @@ public class Linker {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		//import object files into object arrays
+		
+		//check object array validity
+		
+		//create symbol table
+		
+		//populate symbol table
+		
+		//create linker file
+		
+		//transfer text records into linker file and adjust as needed
+		
+		
+		
 
+	}
+	
+	static void checkObjectValidity(ArrayList<ArrayList<String>> objetArray)
+	{
+		
+		//check for header
+		
+		
+		//check the rest of the records  have valid labels and sizes
+		
+		
 	}
 
 }
