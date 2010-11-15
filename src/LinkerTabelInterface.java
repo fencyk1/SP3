b3d899205e90e60205f1fcfe5d9a2ff359d6f4ae
fencyk1 <fencyk.1@buckeyemail.osu.edu>
2010-11-15 10:44:45 -0500

    Started the skeleton for the LinkerTable.

---
+++ src/LinkerTabelInterface.java
@@ -0,0 +1,39 @@
+/**
+ * 
+ */
+
+/**
+ * This class is an object class. It holds the symbol table from the linker. It has three fields:
+ * Name, type, and location, where name is the program or variable name, type is program, ent, ext, etc,
+ * and location is the linker assigned memory location.
+ * 
+ * @author Everett
+ *
+ */
+public interface LinkerTabelInterface {
+
+	/**This method adds a symbole to the symbol table
+	 * 
+	 * @param name = variable name
+	 * @param type = type of variable: program, ent, or ext
+	 * @param location = int value of linker assigned mem address
+	 * @return true if successful, false if name already exists
+	 */
+	boolean add(String name, String type, int location);
+	
+	/**This method changes the location of a pre-existing variable in the table.
+	 * 
+	 * @param name = the name of the variable to be changed
+	 * @param location = the new location
+	 * @return true if successful, false if the variable didn't exist in the table.
+	 */
+	boolean changeLocation(String name, int location);
+	
+	/**This method will return true if the variable is defined in the table.
+	 * 
+	 * @param name = the name of the variable to search for
+	 * @return true if name exists, false if not
+	 */
+	boolean isDefined(String name);
+	
+}
---
+++ src/LinkerTable.java
@@ -0,0 +1,23 @@
+
+
+public class LinkerTable implements LinkerTabelInterface {
+
+	@Override
+	public boolean add(String name, String type, int location) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean changeLocation(String name, int location) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isDefined(String name) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
