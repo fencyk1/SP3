bd8db2c54f1f2232d01c9678cee2ee4a6e24f2bb
fencyk1 <fencyk.1@buckeyemail.osu.edu>
2010-11-15 11:25:28 -0500

    Added more methods to skeleton. Began coding methods.

--- src/LinkerTabelInterface.java
+++ src/LinkerTabelInterface.java
@@ -4,7 +4,7 @@
 
 /**
  * This class is an object class. It holds the symbol table from the linker. It has three fields:
- * Name, type, and location, where name is the program or variable name, type is program, ent, ext, etc,
+ * Name, type, and location, where name is the program or variable name, type is start or ent,
  * and location is the linker assigned memory location.
  * 
  * @author Everett
@@ -12,10 +12,10 @@
  */
 public interface LinkerTabelInterface {
 
-	/**This method adds a symbole to the symbol table
+	/**This method adds a symbol to the symbol table
 	 * 
 	 * @param name = variable name
-	 * @param type = type of variable: program, ent, or ext
+	 * @param type = type of variable: start or ent
 	 * @param location = int value of linker assigned mem address
 	 * @return true if successful, false if name already exists
 	 */
@@ -35,5 +35,14 @@ public interface LinkerTabelInterface {
 	 * @return true if name exists, false if not
 	 */
 	boolean isDefined(String name);
+	
+	/**This method returns the assigned location of the variable name.
+	 * 
+	 * @param name = the name of the variable to retrieve the location for
+	 * @return int location of name
+	 */
+	int getLocation(String name);
+	
+	
 	
 }
--- src/LinkerTable.java
+++ src/LinkerTable.java
@@ -1,23 +1,63 @@
+import java.util.ArrayList;
 
 
 public class LinkerTable implements LinkerTabelInterface {
-
+	private ArrayList<String> symTable = new ArrayList<String>(0);
+	private ArrayList<String> typeTable = new ArrayList<String>(0);
+	private ArrayList<Integer> locTable = new ArrayList<Integer>(0);
+	
 	@Override
 	public boolean add(String name, String type, int location) {
-		// TODO Auto-generated method stub
-		return false;
+		
+		//check if name is already defined
+		if(symTable.contains(name))
+		{
+			return false;
+		}
+		
+		//if name is not already defined, add symbol elements to end of lists
+		symTable.add(name);
+		typeTable.add(type);
+		locTable.add(location);
+		
+		//return true for success
+		return true;
+		
+		
 	}
 
 	@Override
 	public boolean changeLocation(String name, int location) {
-		// TODO Auto-generated method stub
-		return false;
+		
+		//check if name exists in table
+		if (symTable.contains(name))
+		{
+			
+			//set the new location
+			locTable.set(symTable.indexOf(name), location);
+			
+			//return true for success
+			return true;
+		}
+		
+		//else return false
+		else
+		{
+			return false;
+		}
+		
 	}
 
 	@Override
 	public boolean isDefined(String name) {
 		// TODO Auto-generated method stub
 		return false;
+	}
+
+	@Override
+	public int getLocation(String name) {
+		// TODO Auto-generated method stub
+		return 0;
 	}
 
 }
