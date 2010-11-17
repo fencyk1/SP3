0307e4ef65bcea514b965d051eb3eead82fcd4ce
fencyk1 <fencyk.1@buckeyemail.osu.edu>
2010-11-17 15:38:44 -0500

    Added Kyle's changes to checkObjectValidity

--- src/Linker.java
+++ src/Linker.java
@@ -146,38 +146,33 @@ public class Linker {
 	    int textRecs = 0; 
 	    int linkRecs = 0;
 
-	    // int to keep track of the current record position being validated
-	    // will start at one because the first record should be a header record and 
-	    // is being validated anyways
-	    int recordNum = 1;
+	    // boolean values act as switch for whether or not the number of text or linking records are correct
+	    boolean textWrong = false;
+	    boolean linkWrong = false;
 
 	    // create arraylist that holds the first record of the object file
 	    ArrayList<String> firstRec = new ArrayList<String>(objectArray.get(0));
-	    
-	    // holds value of the first character in what should be the header record
-	    char head = firstRec.get(0).charAt(0);
-	    
+
 	    // the program name to be stored; should be consistent throughout the object file
 	    //[linker-adjusted loading address to use for overall linking file address]
 	    String linkLoadAddr = firstRec.get(3);
 
 	    // [execution start address for program]
 	    String execStartAddr = firstRec.get(8);
-	    
+
 	     //[program length]
 	    String pgmLen = firstRec.get(2);
 
 	    //check for header
 
-	    
-
-	   
+	    // holds value of the first character in what should be the header record
+	    Character head = firstRec.get(0).charAt(0);
 
 	    /*
 	     if the first character isn't an 'H' or 'h', or if the length of the header file then
 	     report invalid header file error and abort.
 	    */
-	    if(! (head == 'H' && firstRec.size() == 13))
+	    if(! (head.equals('H') && firstRec.size() == 13))
 	    {
 	        // errors put into linking file??
 	        System.out.println("Error at record 0: INVALID HEADER RECORD! ABORTING LINKING PROCESS");
@@ -195,7 +190,7 @@ public class Linker {
 	    String pgmName = firstRec.get(1);
 
 	    // check module name in second field with program name in 12th field
-	    if(!(pgmName == firstRec.get(12)))
+	    if(!(pgmName.equals(firstRec.get(12))))
 	    {
 	        System.out.println("Error at record 0: module name and program name fields do not match");
 	        isValid = false;
@@ -214,7 +209,7 @@ public class Linker {
 	    }
 
 	    // if the assembler assigned program load address is not in valid hex OR is too long ... 
-	    if(!(conv.isValidHex(linkLoadAddr) && pgmLen.length() <= 4))
+	    if(!(conv.isValidHex(linkLoadAddr) && linkLoadAddr.length() <= 4))
 	    {
 
 	        // throw the error and declare invalidity
@@ -224,7 +219,7 @@ public class Linker {
 	    }
 
 	    // if the execution start address is not in valid hex OR is too long ... 
-	    if(!(conv.isValidHex(execStartAddr) && pgmLen.length() <= 4))
+	    if(!(conv.isValidHex(execStartAddr) && execStartAddr.length() <= 4))
 	    {
 
 	        // throw the error and declare invalidity
@@ -234,7 +229,7 @@ public class Linker {
 	    }
 
 	    // if the number of text records is not in valid hex OR is too long ... 
-	    if(!(conv.isValidHex(firstRec.get(7)) && pgmLen.length() <= 4))
+	    if(!(conv.isValidHex(firstRec.get(7)) && firstRec.get(7).length() <= 4))
 	    {
 
 	        // throw the error and declare invalidity
@@ -250,7 +245,7 @@ public class Linker {
 	    }
 
 	    // if the number of linking records is not in valid hex OR is too long ... 
-	    if(!(conv.isValidHex(firstRec.get(6)) && pgmLen.length() <= 4))
+	    if(!(conv.isValidHex(firstRec.get(6)) && firstRec.get(6).length() <= 4))
 	    {
 
 	        // throw the error and declare invalidity
@@ -304,7 +299,8 @@ public class Linker {
 	    String time = firstRec.get(5);
 
 	    // if the time markings aren't in proper syntax and range ...
-	    if (!(Integer.parseInt(time.substring(0,2))<24 &&
+	    if (!(  time.length() == 8 &&
+	            Integer.parseInt(time.substring(0,2))<24 &&
 	            Integer.parseInt(time.substring(3,5))<60 &&
 	            Integer.parseInt(time.substring(6))<60))
 	    {
@@ -322,32 +318,342 @@ public class Linker {
 	    ArrayList<String> nextRec = new ArrayList<String>(objectArray.get(i));
 
 	    // distinguish the type of record by the letter of the first field
-	    char recType = nextRec.get(0).charAt(0);
+	    Character recType = nextRec.get(0).charAt(0);
 
-	        if(recType == 'L')
+	        if(recType.equals('L'))
 	        {
 
-	        // validate the linking record
+	            /*
+	             * if a linking record is encountered, it should be between a header and an end record.
+	             * thus, hasHeader should be true and hasEnded should be false
+	             */
+
+	            // if the above requirements isn't true ...
+	            if(!(hasHeader && !hasEnded))
+	            {
+
+	                // throw the error and "devalidate" the object file
+	                System.out.println("Error at record " + i + ":misplaced linking record");
+	                isValid = false;
+	            }
+
+	            // if the number of linking records given in the header record is exceeded, 
+	            // and the error switch is not thrown yet ...
+	            if(linkRecs <= 0 && !(linkWrong))
+	            {
+
+	                // turn the switch, throw the error, and "devalidate" the object file
+	                linkWrong = true;
+	                System.out.println("Error at record " + i + ":number of linking records given by header "
+	                         + "record is exceeded.");
+	                isValid = false;
+	            }
+	            else
+	            {
+
+	            // otherwise, decrement the number of linking records to find and validate the linking record
+	            linkRecs--;
+	            }
+
+	            // store the value of the name of the entry in the object file
+	            String entryName = nextRec.get(1);
+
+	            // TODO check label for proper syntax. If not in proper syntax, throw error and devalidate the object file
+
+	            // store the address of the name of the entry in the object file
+	            String entryAddress = nextRec.get(2);
+
+	            // if the address is not in proper syntax or if the memory bounds are violated ...
+	            if(!(conv.isValidHex(entryAddress)))
+	            {
+
+	                // throw the error and "devalidate" the object file
+	                System.out.println("Error at record " + i + ":address of linking record entry is not in proper syntax");
+	                isValid = false;
+	            }
+	            else if (!(Integer.parseInt(conv.hexToDec(entryAddress)) <= 65535))
+	            {
+
+	                // throw the error and "devalidate" the object file
+	                System.out.println("Error at record " + i + ":address of linking record entry is outside memory bounds");
+	                isValid = false;
+	            }
+
+	            // store the value of the type of the entry in the object file
+	            String entryType = nextRec.get(3);
+
+	            // if the type of the entry does not equal 'start' or 'ent' ...
+	            if(!(entryType.equals("start") || entryType.equals("ent")))
+	            {
+
+	                // throw the invalid entry type error and "devalidate" the object file
+	                System.out.println("Error at record " + i + ":invalid entry type");
+	                isValid = false;
+	            }
+
+	            // if the program name in the linking record doesn't match the given program name in the header record ...
+	            if(!(nextRec.get(3).equals(pgmName)))
+	            {
+
+	                // throw the mismatched program name error and "devalidate" the object file
+	                System.out.println("Error at record " + i + ":program name in linking record does not match program"
+	                         + " name in corresponding header record");
+	                isValid = false;
+	            }
 	        }
 
-	        else if(recType == 'T')
+	        else if(recType.equals('T'))
 	        {
 
-	        // validate the text record
+	            /*
+	             * if a text record is encountered, it should be between a header and an end record.
+	             * thus, hasHeader should be true and hasEnded should be false
+	             */
+
+	            // if the above requirements isn't true ...
+	            if(!(hasHeader && !hasEnded))
+	            {
+
+	                // throw the error and "devalidate" the object file
+	                System.out.println("Error at record " + i + ":misplaced text record");
+	                isValid = false;
+	            }
+
+	            // if the number of text records given in the header record is exceeded, 
+	            // and the error switch is not thrown yet ...
+	            if(linkRecs <= 0 && !(textWrong))
+	            {
+	                // turn the switch, throw the error, and "devalidate" the object file
+	                textWrong = true;
+	                System.out.println("Error at record " + i + ":number of text records given by header "
+	                         + "record is exceeded.");
+	                isValid = false;
+	            }
+	            else
+	            {
+	            // otherwise, decrement the number of linking records to find and validate the text record
+	            textRecs--;
+	            }
+
+	            // TODO validation for text records
 	        }
 
-	        else if(recType == 'E')
+	        else if(recType.equals('E'))
 	        {
 
-	        // validate the end record
+	        // TODO boolean switching
+	            /*
+	             * if an end record is encountered, it should be after a header record.
+	             * thus, hasHeader should be true and hasEnded should be false at this point
+	             */
+
+	            // if the above requirements isn't true ...
+	            if(!(hasHeader && !hasEnded))
+	            {
+
+	                // throw the error and "devalidate" the object file
+	                System.out.println("Error at record " + i + ":misplaced end record or missing header record");
+	                isValid = false;
+	            }
+
+	            // if the above requirements are true ...
+	            else
+	            {
+
+	                // change the boolean values to reflect what should be the end of the object file
+	                hasHeader = false;
+	                hasEnded = true;
+	            }
+	            // if the hex field for total number of records isn't in proper hex ...
+	            if(!(conv.isValidHex(nextRec.get(1)) && nextRec.get(1).length() == 4))
+	            {
+
+	                // throw the error and "devalidate" the object file
+	                System.out.println("Error at record " + i + ": \"total number of records\" field is not in proper syntax");
+	                isValid = false;
+	            }
+
+	            // create and store value for total number of records for validation
+	            int recNum = Integer.parseInt(nextRec.get(1));
+
+	            // of the total number of records given by the end record is not equal to
+	            // the literal number of records in the entire object file ...
+	            if(!(objectArray.size() == recNum))
+	            {
+	                // throw the error and "devalidate" the object file
+	                System.out.println("Error at record " + i + ":total number of records given by the end record is "
+	                        + "not equal to the total number of records in the object file!");
+	                isValid = false;
+	            }
+
+	            // if the program name in the end record doesn't match the given program name in the header record ...
+	            if(!(nextRec.get(2).equals(pgmName)))
+	            {
+
+	                // throw the mismatched program name error and "devalidate" the object file
+	                System.out.println("Error at record " + i + ":program name in end record does not match program"
+	                         + " name in corresponding header record");
+	                isValid = false;
+	            }
+
 	        }
 
+	        else if(recType.equals('H'))
+	        {
+
+	            // TODO boolean check for duplicate header records
+
+	            // throw an error for duplicate header record
+	            System.out.println("Error at record " + i + ": another header record detected in object file.");
+	            isValid = false;
+
+	    // the program name to be stored; should be consistent throughout the object file
+	    //[linker-adjusted loading address to use for overall linking file address]
+	    String linkLoadAddrs = nextRec.get(3);
+
+	    // [execution start address for program]
+	    String execStartAddrs = nextRec.get(8);
+
+	     //[program length]
+	    String pgmLens = nextRec.get(2);
+
+	    // first things first, setting the switches correctly. This object file has a header record
+	    // and has not run into an end record yet.
+	    hasHeader = true;
+	    hasEnded = false;
+
+	    // save the name of the program
+	    pgmName = nextRec.get(1);
+
+	    // check module name in second field with program name in 12th field
+	    if(!(pgmName.equals(nextRec.get(12))))
+	    {
+	        System.out.println("Error at record " + i + ": module name and program name fields do not match");
+	        isValid = false;
+	    }
+
+	    // check validity of each hex field, then verify the length
+
+	    // if the program length is not in valid hex OR is too long ... 
+	    if(!(conv.isValidHex(pgmLens) && pgmLens.length() <= 4))
+	    {
+
+	        // throw the error and declare invalidity
+	        System.out.println("Error at record " + i + ": program length field is not in valid syntax: "
+	                + " \"hhhh\" where h is a valid hexadecimal number");
+	        isValid = false;
+	    }
+
+	    // if the assembler assigned program load address is not in valid hex OR is too long ... 
+	    if(!(conv.isValidHex(linkLoadAddrs) && linkLoadAddrs.length() <= 4))
+	    {
+
+	        // throw the error and declare invalidity
+	        System.out.println("Error at record " + i + ": assembler assigned program load address"
+	                + " field is not in valid syntax: \"hhhh\" where h is a valid hexadecimal number");
+	        isValid = false;
+	    }
+
+	    // if the execution start address is not in valid hex OR is too long ... 
+	    if(!(conv.isValidHex(execStartAddrs) && execStartAddrs.length() <= 4))
+	    {
+
+	        // throw the error and declare invalidity
+	        System.out.println("Error at record " + i + ": execution start address field is not in"
+	                + " valid syntax: \"hhhh\" where h is a valid hexadecimal number");
+	        isValid = false;
+	    }
+
+	    // if the number of text records is not in valid hex OR is too long ... 
+	    if(!(conv.isValidHex(nextRec.get(7)) && nextRec.get(7).length() <= 4))
+	    {
+
+	        // throw the error and declare invalidity
+	        System.out.println("Error at record " + i + ": number of text records field is not in"
+	                + " valid syntax: \"hhhh\" where h is a valid hexadecimal number");
+	        isValid = false;
+	    }
+	    else
+	    {
+
+	        // save the value for the number of text records expected
+	        textRecs = Integer.parseInt(conv.hexToDec(nextRec.get(7)));
+	    }
+
+	    // if the number of linking records is not in valid hex OR is too long ... 
+	    if(!(conv.isValidHex(nextRec.get(6)) && nextRec.get(6).length() <= 4))
+	    {
+
+	        // throw the error and declare invalidity
+	        System.out.println("Error at record " + i + ": number of linking records field is not in"
+	                + " valid syntax: \"hhhh\" where h is a valid hexadecimal number");
+	        isValid = false;
+	    }
+	    else
+	    {
+
+	        // save the value for the number of linking records expected
+	        linkRecs = Integer.parseInt(nextRec.get(6));
+	    }
+
+	    // --------check the date field for proper syntax and values--------- //
+
+	    // string for the year portion of the date field
+	    String years = nextRec.get(4).substring(0, 4);
+
+	    // if the year is too far in the future ...
+	    if(Integer.parseInt(years) > 2010)
+	    {
+
+	        //  give a [funny ;} ] warning
+	        System.out.println("Warning: given year is too far in the future!"
+	                + "\nRecommended action: give Marty McFly his time machine back.");
+	    }
+
+	     // if the year is too far in the past ...
+	    else if(Integer.parseInt(years) < 2010)
+	        {
+
+	            //  give a [funny ;} ] warning
+	            System.out.println("Warning: given year is too far in the past!"
+	                    + "\nRecommended action: borrow Marty McFly's time machine and go back to the future.");
+	        }
+
+	    // string for the day portion of the date field
+	    String days = nextRec.get(4).substring(5);
+
+	    // check bounds for the day; anything else that should be checked ???
+	    if(!(Integer.parseInt(days) >= 0 && Integer.parseInt(days) <= 365))
+	    {
+
+	        //  give a [funny ;} ] warning
+	        System.out.println("Warning: given day is outside reasonable bounds for the day."
+	                + "\nRecommended action: check syntax, or if in a leap year, please try again tomorrow! =)");
+	    }
+
+	    // check the time field for proper syntax
+	    String times = nextRec.get(5);
+
+	    // if the time markings aren't in proper syntax and range ...
+	    if (!(  times.length() == 8 &&
+	            Integer.parseInt(times.substring(0,2))<24 &&
+	            Integer.parseInt(times.substring(3,5))<60 &&
+	            Integer.parseInt(times.substring(6))<60))
+	    {
+
+	    //  give a [funny ;} ] warning
+	        System.out.println("Warning: given time is not in proper syntax or range: hh:mm:ss where hh == hours, mm == minutes, and ss == seconds."
+	                + "\nRecommended action: check syntax, or buy your assembler a watch! =P");
+	    }
+
+	        }
 	        else
 	        {
 
 	        // invalid record
 	        }
 	    }
+
 	    // if the validation has not aborted by this point, the object file is good enough to link
 	    // therefore the method will return true if it makes it to this point
 	    return true;
@@ -360,9 +666,6 @@ public class Linker {
 	            - decrement textRecs and linkRecs each time one of the corresponding text/linking records are found
 	            - if either get to zero: 
 	                -throw an error [naming convention for error ???]
-	                - ?? skip rest of corresponding files ??
-
-	        ----> what else for header records ?? ???
 	    */
 	    //check the rest of the records  have valid labels and sizes
 
